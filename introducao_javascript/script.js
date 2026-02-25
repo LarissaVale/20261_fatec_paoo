e1c23acd90960f2a4fb7aa28cb85fdee4f69ecf6
Larissa Vitória Nogueira do Vale <larissavitorianogueira13@gmail.com>
2026-02-25 18:09:54 -0300

    learn: encerra codigo

--- introducao_javascript/script.js
+++ introducao_javascript/script.js
@@ -1,1 +1,64 @@
-console.log('Hello, JS!')
+//declaração de variáveis
+//const, let e var
+
+//const nome = 'José'
+//console.log(nome)
+
+//let nome = 'Maria'
+//console.log(nome)
+//nome = `Meu nome é ${nome}.`
+
+//console.log('Antes do for')
+//for (const i = 0; i< 10; i = i + 1){
+//    console.log('for com const...')
+//}
+//console.log('Depois do for')
+
+//var linguagem = 'Javascript'
+//console.log(`Aprendendo ${linguagem}`)
+//var linguagem = 'Java'
+//console.log('Aprendendo ' + linguagem)
+
+//var idade = 18
+//console.log(`Oi, ${nome}`)
+//if(idade >= 18){
+//    var nome = 'Maria'
+//    console.log(`Parabéns, ${nome}. Você pode dirigir.`)
+//}
+//console.log('Até mais, ' + nome + '.' )
+
+//tipos
+//js é dinamicamente tipado
+//let a = 2
+//console.log(typeof(a))
+//a = true
+//console.log(typeof(a))
+
+//coerção implicita
+//const n1 = 2
+//const n2 = '3'
+//const n3 = n1 + n2
+//console.log(`${n1} + ${n2} = ${n3}`)
+
+//coerção explicita
+//const n1 = 2
+//const n2 = '3'
+//const n3 = n1 + Number(n2)
+//console.log(`${n1} + ${n2} = ${n3}`)
+
+//comparação por igualdade
+//java: ==, Python: ==
+//javascript: == ou ===(usamos apenas esse)
+//console.log(1 == 1) true
+//console.log(1 === 1) false - são de tipos iguais? se sim, compara valor, se não, não compara valor
+//console.log(1 == '1') true
+//console.log(true == 1) true
+//console.log(1 == [1]) true
+//console.log(null == null) true
+//null e undefined
+//exemplos:
+//a = null
+//b = undefined
+//console.log(a === b)
+//console.log(false == []) true
+//console.log([] == []) false                  
